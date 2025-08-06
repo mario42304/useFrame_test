1aa7a0660fbd3be2c45e074f41701bf8565e6ee2
mario42304 <gotohda42@gmail.com>
2025-08-06 16:55:19 +0900

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,93 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import * as THREE from "three";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface MyMeshProps {
+  isRunning: boolean;
+  speed: number;
 }
 
-export default App
+function MyMesh({ isRunning, speed }: MyMeshProps) {
+  const ref = useRef<THREE.Object3D>(null);
+
+  useFrame(() => {
+    if (ref.current && isRunning) {
+      ref.current.rotation.y += 0.01 + speed;
+    }
+  });
+
+  return (
+    <mesh ref={ref}>
+      <boxGeometry attach="geometry" args={[2, 2, 2]} />
+      <meshPhongMaterial attach="material" color="hotpink" />
+    </mesh>
+  );
+}
+
+export default function App() {
+  const [isRunning, setIsRunning] = useState<boolean>(false);
+  const [speed, setSpeed] = useState<number>(0);
+
+  const onChangeRunning = () => {
+    setIsRunning(!isRunning);
+  };
+  const onChangeSpeed = (operator: string) => {
+    switch (operator) {
+      case "+":
+        setSpeed((prev) => prev + 0.01);
+        break;
+      case "-":
+        setSpeed((prev) => prev - 0.01);
+        break;
+      default:
+        break;
+    }
+  };
+  return (
+    <>
+      <Canvas>
+        <ambientLight intensity={0.1} />
+        <directionalLight
+          position={[1, 1, 1]}
+          lookAt={[0, 0, 0]}
+          intensity={1}
+        />
+        <MyMesh isRunning={isRunning} speed={speed} />
+      </Canvas>
+      <button onClick={onChangeRunning}>{isRunning ? "stop" : "start"}</button>
+      <button onClick={() => onChangeSpeed("+")}>speed+</button>
+      <button onClick={() => onChangeSpeed("-")}>speed-</button>
+    </>
+  );
+}
+
+// function MyRotatingBox() {
+//   const ref = useRef<THREE.Object3D>(null);
+
+//   useFrame(({ clock }) => {
+//     if (ref.current) {
+//       const a = clock.getElapsedTime();
+//       ref.current.rotation.x = a;
+//     }
+//   });
+
+//   return (
+//     <mesh ref={ref}>
+//       <boxGeometry />
+//       <meshPhongMaterial color="royalblue" />
+//     </mesh>
+//   );
+// }
+
+// export default function App() {
+//   return (
+//     <div className="App">
+//       <Canvas>
+//         <MyRotatingBox />
+//         <ambientLight intensity={0.1} />
+//         <directionalLight />
+//       </Canvas>
+//     </div>
+//   );
+// }
